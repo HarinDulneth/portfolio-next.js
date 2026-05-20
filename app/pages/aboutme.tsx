ee1268bfd4c9e7486e280166a05b84b8403ecab4
HarinDulneth <harindulneth@gmail.com>
2026-05-21 02:19:11 +0530

    update cv

--- app/pages/aboutme.tsx
+++ app/pages/aboutme.tsx
@@ -152,11 +152,12 @@ export default function About() {
                       <div className="h-px bg-white/10 transition-colors duration-300" />
                     </div>
                     <p className="relative z-10 font-inter text-white/75 text-sm sm:text-base md:text-lg leading-relaxed">
-                      I'm Harin Dulneth, a Computer Science undergraduate specializing in Artificial Intelligence with a
-                      strong interest in AI developments and applied Machine
-                      Learning and a solid foundation in full-stack development.
-                      I truly enjoy teamwork - collaborating to solve complex
-                      problems and build innovative solutions.
+                      I'm Harin Dulneth, a Computer Science undergraduate
+                      specializing in Artificial Intelligence with a strong
+                      interest in AI developments and applied Machine Learning
+                      and a solid foundation in full-stack development. I truly
+                      enjoy teamwork - collaborating to solve complex problems
+                      and build innovative solutions.
                     </p>
                   </div>
                 </div>
